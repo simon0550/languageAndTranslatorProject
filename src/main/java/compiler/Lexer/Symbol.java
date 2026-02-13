aec8a207e834d21dda7ba4920bcfa33e2b32d1ac
Simon <simon.bosseler@student.vinci.be>
2026-02-13 15:17:49 +0100

    feat : Add token and attribute field

--- src/main/java/compiler/Lexer/Symbol.java
+++ src/main/java/compiler/Lexer/Symbol.java
@@ -1,4 +1,22 @@
 package compiler.Lexer;
 
 public class Symbol {
+  private String token;
+  private Object attribute;
+
+  public Symbol(Object attribute, String token) {
+    this.attribute = attribute;
+    this.token = token;
+  }
+
+  public Symbol(String token) {
+    this.token = token;
+  }
+
+  @Override
+  public String toString() {
+    if(attribute != null){
+      return "<" + token + "," + attribute + ">";
+    }else return "<" + token + ">";
+  }
 }
