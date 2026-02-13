ec28849b548289fb665cdbba7c34e36a05068b45
Simon <simon.bosseler@student.vinci.be>
2026-02-13 15:27:31 +0100

    feat : Add Lexer field and constructor

--- src/main/java/compiler/Lexer/Lexer.java
+++ src/main/java/compiler/Lexer/Lexer.java
@@ -1,9 +1,19 @@
 package compiler.Lexer;
+import java.io.IOException;
 import java.io.Reader;
 
 public class Lexer {
-    
+
+    Reader reader;
+    private int current;
+
     public Lexer(Reader input) {
+        this.reader = input;
+        try {
+            this.current = reader.read();
+        }catch (IOException e){
+            e.toString();
+        }
     }
     
     public Symbol getNextSymbol() {
