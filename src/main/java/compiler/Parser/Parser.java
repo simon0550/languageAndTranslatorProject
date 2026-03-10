d332b74e101a8e3c73bec4fb436853bc193387d5
Victor Morabito <victor.morabito@student.vinci.be>
2026-03-10 14:21:22 +0100

    feat: added variable declaration to parser main class

--- src/main/java/compiler/Parser/Parser.java
+++ src/main/java/compiler/Parser/Parser.java
@@ -1,8 +1,13 @@
 package compiler.Parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import compiler.Lexer.Lexer;
 import compiler.Lexer.Symbol;
-
+import compiler.Parser.IntNode;
+import compiler.Parser.StringNode;
+import compiler.Parser.ProgramNode;
 public class Parser {
 
   private Lexer lexer;
@@ -13,7 +18,62 @@ public class Parser {
     this.lexer = lexer;
   }
 
-  public Node getAST(){
-    return null;
+  public Node getAST() {
+    // Si on voit un type (int, float...), on traite une déclaration
+    List<Node> instructions = new ArrayList<>();
+    while (this.symbol != null){
+      instructions.add(parse());
+      step();
+    }
+    return new ProgramNode(instructions);
+    
+  
   }
+
+  private Node parse(){
+    // here, we have to put all our parser rules 
+    if (this.symbol.getToken().equals("type")){
+      return parseVariableDeclaration(this.symbol);
+    }
+    else return new EmptyNode();
+
+  }
+
+  // This only manges varible declaration and the values associated
+  public Node parseVariableDeclaration(Symbol symbol){
+    String type = symbol.getToken();
+    step();
+    String identifier = this.symbol.getToken();
+    step();
+    String assignmentOperator = this.symbol.getToken(); 
+    if(!assignmentOperator.equals("="))
+      throw new RuntimeException("Syntax error: expected '=' after variable name");
+    step();
+    Node expression = parseExpression(type);
+    return new VariableDeclarationNode(type, identifier, expression);
+  } 
+
+  private Node parseExpression(String type) {
+    if(type.equals ("int")){
+      int value = Integer.parseInt(this.symbol.getAttribute().toString());
+      return new IntNode(value);
+    } else if (type.equals("float")){
+      float value = Float.parseFloat(this.symbol.getAttribute().toString());
+      return new FloatNode(value);
+    } else if (type.equals("bool")){
+      boolean value = Boolean.parseBoolean(this.symbol.getAttribute().toString());
+      return new BoolNode(value);
+    }
+    return new EmptyNode();
+    
+  }
+
+
+  
+
+  public void step(){
+    this.symbol = lexer.getNextSymbol();
+  }
+
+
 }
