edf6f2e410305b378b0dfabee9c0ec7e19d9c592
Simon <simon.bosseler@student.vinci.be>
2026-03-30 15:18:59 +0200

    feat: add ProgramNode case in SemanticAnalyzer

--- src/main/java/compiler/SemanticAnalyser/SemanticAnalyzer.java
+++ src/main/java/compiler/SemanticAnalyser/SemanticAnalyzer.java
@@ -1,9 +1,30 @@
 package compiler.SemanticAnalyser;
 
+import compiler.Parser.Node;
+import compiler.Parser.ProgramNode;
+
 public class SemanticAnalyzer {
   private SymbolTable symbolTable;
 
   public SemanticAnalyzer() {
     this.symbolTable = new SymbolTable();
+  }
+
+  public void analyseTree(Node root){
+    browse(root);
+  }
+
+  private void browse(Node node){
+    if (node == null) return;
+
+    if(node instanceof ProgramNode){
+      browseProgramNode((ProgramNode) node);
+    }
+  }
+
+  private void browseProgramNode(ProgramNode programNode){
+    for(Node node : programNode.getDeclarations()){
+      browse(node); // Appel récursif (on s'enfonce dans l'arbre)
+    }
   }
 }
