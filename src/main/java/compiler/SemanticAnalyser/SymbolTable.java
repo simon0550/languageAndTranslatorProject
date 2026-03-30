7b7c4224760b9c3337008d6686580bdddf8b8fb9
Simon <simon.bosseler@student.vinci.be>
2026-03-30 15:08:56 +0200

    feat: add stack in SymbolTable class

--- src/main/java/compiler/SemanticAnalyser/SymbolTable.java
+++ src/main/java/compiler/SemanticAnalyser/SymbolTable.java
@@ -2,11 +2,26 @@ package compiler.SemanticAnalyser;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class SymbolTable {
-  private Map<String, String> mapScopes;
+
+  // Pile pour les couches de scopes, mieux ?
+  // Permet d'avoir des variables de même nom dans scopes différents
+  private Stack<Map<String,String>> stackScopes = new Stack<>();
 
   public SymbolTable() {
-    this.mapScopes = new HashMap<>();
+    this.stackScopes.push(new HashMap<>());
+  }
+
+  public void addNewScope(){
+    stackScopes.push(new HashMap<>());
+    // On pourrait rajouter les comportements ici ?
+  }
+
+  public void removeScope(){
+    if(stackScopes.size() > 1){
+      stackScopes.pop();
+    }
   }
 }
